c93de79ff28af86f9d9cd91ea83f8109dfbd80cf
ZhangMC <zbsqqyx@qq.com>
2018-03-08 10:12:44 +0800

    试一试大神的东西好不好用

---
+++ SuperKeyboardSimulate/SuperKeyboardSimulate.py
@@ -0,0 +1,15 @@
+import keyboard
+import mouse
+
+import time
+time.sleep(2)  
+
+#keyboard.press_and_release('shift+s, space')
+
+mouse.move(1000,100,absolute=True,duration=0)
+time.sleep(2)
+
+
+time.sleep(2)
+
+zzz=1
